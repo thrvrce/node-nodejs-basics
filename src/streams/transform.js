e773b88f8654fab93a7d36a950d3dde4997c7e3f
VA <avdeevvictor95@gmail.com>
2025-11-01 00:53:54 +0400

    feat: add streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-  // Write your code here
+	try {
+    const reverseStream = new Transform({
+      transform(chunk, _encoding, callback) {
+        const reversed = chunk.toString().split('').reverse().join('');
+        callback(null, reversed);
+      }
+    });
+
+    await pipeline(process.stdin, reverseStream, process.stdout);
+  } catch {
+    throw new Error('Stream operation failed');
+  }
 };
 
 await transform();
